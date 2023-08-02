5cbd826b12fae04cff1986c18ccdc42d4cdafded
Hokome <xrhokom@gmail.com>
2023-08-02 16:10:23 +0200

    Added initializer path check and crash

--- Assets/Essentials/Initializer.cs
+++ Assets/Essentials/Initializer.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+using Object = UnityEngine.Object;
+
 namespace AssetFactory
 {
-
     [CreateAssetMenu(fileName = "Initializer", menuName = "Initializer", order = 1)]
     public class Initializer : ScriptableObject
     {
-        const string INITIALIZER_PATH = "Assets/Initializer.asset";
+        const string INITIALIZER_PATH = "Assets/Essentials/Initializer.asset";
 
         [SerializeField] private Object[] initializeOnSceneLoad = new Object[0];
 
@@ -17,6 +19,15 @@ namespace AssetFactory
         private static void InitializeObjects()
         {
             Initializer obj = AssetDatabase.LoadAssetAtPath<Initializer>(INITIALIZER_PATH);
+            if (obj == null)
+            {
+                Debug.LogError("The initializer object is null. Check the path of the initializer.");
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+#else
+                UnityEngine.Diagnostics.Utils.ForceCrash(UnityEngine.Diagnostics.ForcedCrashCategory.FatalError);
+#endif
+            }
             if (obj.initializeOnSceneLoad == null) return;
             for (int i = 0; i < obj.initializeOnSceneLoad.Length; i++)
             {
